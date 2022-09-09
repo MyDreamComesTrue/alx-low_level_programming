cbec8b40a756736c0be0b13fd65c4c7a415a431d
MyDreamComesTrue <aziaboukoami@gmail.com>
2022-09-08 17:00:16 -0700

    Resolve C programming, task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * Return: 0
+ * Return: Always 0
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	Return (0);
+	return (0);
 }
