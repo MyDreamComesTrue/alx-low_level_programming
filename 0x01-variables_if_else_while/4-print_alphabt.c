dd050a57f28a6bfe6b495e14bb1b55798bcfe80d
MyDreamComesTrue <aziaboukoami@gmail.com>
2022-09-09 16:06:13 -0700

    Resolve task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
@@ -15,7 +13,7 @@ int main(void)
 	{
 		if (c != 'q' && c != 'e')
 			putchar(c);
-			c++;
+		c++;
 	}
 	putchar('\n');
 
