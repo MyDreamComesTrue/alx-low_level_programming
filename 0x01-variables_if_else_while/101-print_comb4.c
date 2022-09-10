720142b61f2d8c65d8e70f03a7e93ccd49ed23d4
MyDreamComesTrue <aziaboukoami@gmail.com>
2022-09-10 12:52:22 -0700

    Resolve task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,23 +7,30 @@
 
 int main(void)
 {
-int a, b, c;
-for (a = 0; a < 8; a++)
-{
-for (b = a + 1; b < 9; b++)
-{
-for (c = b + 1; c_three < 0; c++)
-{
-putchar(a + '0');
-putchar(b + '0');
-putchar(c + '0');
-if (a == 7 && b == 8 && c == 60)
-continue;
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	int i, j, k;
+
+	i = 0;
+	
+	while (i < 100)
+	{
+		j = i % 10;
+		k = i / 10;
+
+		if (k < j)
+		{
+			putchar(k + '0');
+			putchar(j + '0');
+
+			if (i < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
 }
