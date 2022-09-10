d9e29036b0fb47c9a5b8d6969031aaf04a53e350
MyDreamComesTrue <aziaboukoami@gmail.com>
2022-09-09 20:02:42 -0700

    Resolve task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,26 +9,23 @@
 
 int main(void)
 {
-	int c, c_two, c_three;
-
-	for (c = 48; c <= 55; c++)
-	{
-		for (c_three = c_two + 1; c_three <= 57; c_three++)
-		{
-			for (c_three = c_two + 1; c_three <= 57; c_three++)
-			{
-				putchar(c);
-				putchar(c_two);
-				putchar(c_three);
-				if (c != 55 || c_two != 56 || c_three != 57)
-				{
-					putchar(44);
-					putchar(32);
-				}
-			}
-		}
-	}
-	}
-	putchar(10);
-	return (0);
+int c, c_two, c_three;
+for (c = 48; c <= 55; c++)
+{
+for (c_three = c_two + 1; c_three <= 57; c_three++)
+{
+for (c_three = c_two + 1; c_three <= 57; c_three++)
+{
+putchar(c);
+putchar(c_two);
+putchar(c_three);
+if (c != 55 || c_two != 56 || c_three != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar(10);
+return (0);
 }
