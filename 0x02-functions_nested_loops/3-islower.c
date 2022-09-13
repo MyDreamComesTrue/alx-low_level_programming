96aa406eccbb42178f32ee4709521dfc9073ef81
MyDreamComesTrue <aziaboukoami@gmail.com>
2022-09-13 16:49:53 -0700

    Resolve C - Functions and nested loops, task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ *
+ * Return: 0, if wrong
+ * 
+ * Return: 1, if right
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z'; c++)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
