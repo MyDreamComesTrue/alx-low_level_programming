f9e9c52ef3dab21f16434027b7cb40a2cec16fdf
MyDreamComesTrue <aziaboukoami@gmail.com>
2022-09-13 16:14:24 -0700

    Resolve C - Functions and nested loops, task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet x10, in lowercase
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	char alphabet;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+		_putchar(alphabet);
+		}
+	}
+	_putchar('\n');
+}
